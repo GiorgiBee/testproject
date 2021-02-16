b9b6822935452019e70bd1c9bc247dbfb1c0ac2d
giorgieremashvili <eremashvili.g123@gmail.com>
2021-02-16 20:34:25 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -8,7 +8,16 @@ import Button from '@material-ui/core/Button';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
 
+
 function App() {
+  function onchange(id,checked){
+    if(checked){
+      document.getElementById(`todo_text_${id}`).style.textDecoration = "line-through";
+    }
+    else{
+      document.getElementById(`todo_text_${id}`).style.textDecoration = "none";
+    }   
+  }
   return (
     <Container maxWidth="sm">
     <div className="formgroup">
@@ -31,10 +40,10 @@ function App() {
         </div>
       <div className="todo">
         <div>Todo List</div>
-        <div><Checkbox></Checkbox>Test<Button  variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
-        <div><Checkbox></Checkbox>Write<Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
-        <div><Checkbox></Checkbox>Finalize<Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
-        <div><Checkbox></Checkbox>Book<Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(1,e.target.checked)}></Checkbox><p id="todo_text_1">Text</p></div><Button  variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(2,e.target.checked)}></Checkbox><p id="todo_text_2">Write</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(3,e.target.checked)}></Checkbox><p id="todo_text_3">Finalize</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(4,e.target.checked)}></Checkbox><p id="todo_text_4">Work</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
         </div>   
     </Container>
     
