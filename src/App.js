62ce1fe710ee0fcd5b74aeb82fedadf6b4ad4f42
giorgieremashvili <eremashvili.g123@gmail.com>
2021-02-20 17:41:20 +0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import './App.css';
+import {
+  useEffect
+} from 'react'
 import Container from '@material-ui/core/Container';
 import TextField from '@material-ui/core/TextField';
 import { Checkbox, InputAdornment } from '@material-ui/core';
@@ -7,9 +10,16 @@ import FormatListBulletedIcon from '@material-ui/icons/FormatListBulleted';
 import Button from '@material-ui/core/Button';
 import AddCircleOutlineIcon from '@material-ui/icons/AddCircleOutline';
 import DeleteOutlineOutlinedIcon from '@material-ui/icons/DeleteOutlineOutlined';
-
+import {useDispatch, useSelector} from "react-redux";
+import {AddTodos} from "./Redux/actions";
 
 function App() {
+  const {
+    todos
+  } = useSelector(state => state)
+  
+  const dispatch = useDispatch();
+
   function onchange(id,checked){
     if(checked){
       document.getElementById(`todo_text_${id}`).style.textDecoration = "line-through";
@@ -17,6 +27,14 @@ function App() {
     else{
       document.getElementById(`todo_text_${id}`).style.textDecoration = "none";
     }   
+  }
+  
+  useEffect(() => {
+    console.log(todos);
+  }, [todos])
+
+  const AddTodo=() => {
+    dispatch(AddTodos(document.getElementById("outlined-basic").value))
   }
   return (
     <Container maxWidth="sm">
@@ -36,14 +54,22 @@ function App() {
         }
       />
       
-        <Button className="Button" variant="contained" color="primary" startIcon={<AddCircleOutlineIcon/>}>Add</Button>
+        <Button className="Button" variant="contained" color="primary" onClick={(e)=> AddTodo()} startIcon={<AddCircleOutlineIcon/>}>Add</Button>
+        
         </div>
       <div className="todo">
         <div>Todo List</div>
-        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(1,e.target.checked)}></Checkbox><p id="todo_text_1">Text</p></div><Button  variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        {
+          todos.length > 0 && (
+            todos.map(item => {
+              return <div className="todo_item"><div><Checkbox onChange={(e) => onchange(2,e.target.checked)}></Checkbox><p id="todo_text_2">{item}</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+            })
+          )
+        }
+        {/* <div className="todo_item"><div><Checkbox onChange={(e) => onchange(1,e.target.checked)}></Checkbox><p id="todo_text_1">Text</p></div><Button  variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
         <div className="todo_item"><div><Checkbox onChange={(e) => onchange(2,e.target.checked)}></Checkbox><p id="todo_text_2">Write</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
         <div className="todo_item"><div><Checkbox onChange={(e) => onchange(3,e.target.checked)}></Checkbox><p id="todo_text_3">Finalize</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
-        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(4,e.target.checked)}></Checkbox><p id="todo_text_4">Work</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div>
+        <div className="todo_item"><div><Checkbox onChange={(e) => onchange(4,e.target.checked)}></Checkbox><p id="todo_text_4">Work</p></div><Button variant="outlined" startIcon={<DeleteOutlineOutlinedIcon />}></Button></div> */}
         </div>   
     </Container>
     
